65c38f29632dbd996414736d9c1489b1fe331be3
Glend Maatita <glend.maatita@gmail.com>
2016-04-09 11:27:36 +0700

    init commit

---
+++ app/src/main/java/iakupn/iakupn2016/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package iakupn.iakupn2016.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import iakupn.iakupn2016.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
